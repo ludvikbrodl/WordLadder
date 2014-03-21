2a9a928189c01f1302846f0b94a99745806b1dab
Ludvik Brodl <ludvik.brodl@gmail.com>
2014-03-21 09:55:28 +0100

    an halvtaskig bfs, söker igenom alla orden och skriver ut noderna i den ordning den traveserar, dock klyddar det lite när det är rundgång, den går en gång för mycket. tror vi måste använda det här med noder och använda en visited flagga på varje nod: (våra noder är words i vårt fall)

--- WordLadder/src/main.java
+++ WordLadder/src/main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import java.util.Queue;
 import java.util.Set;
 
@@ -27,52 +28,57 @@ public class main {
 			tempWord = words.get(i);
 			map.put(tempWord, new LinkedList<Word>());
 		}
-	
-		//lägg till alla 4:a bokstäver långa ord till bucketsen.
+
+		// lägg till alla 4:a bokstäver långa ord till bucketsen.
 		Set<Word> keySet = map.keySet();
 		System.out.println(keySet.size());
 		for (Word key : keySet) {
 			for (int i = 0; i < words.size(); i++) {
 				Word word = words.get(i);
-				if (word.checkMatch(key.getKey()) && !word.toString().equals(key.toString())) {
+				if (word.checkMatch(key.getKey())
+						&& !word.toString().equals(key.toString())) {
 					map.get(key).add(word);
 				}
 			}
 		}
-	
-		
 
-		//		ArrayList<LinkedList<Word>> ord = new ArrayList<LinkedList<Word>>();
-//		
-//		for (Word w : words) {
-//			ord.add(map.get(w.getKey()));
-//		}
-//		
-//		System.out.println(ord.toString());
+		// ArrayList<LinkedList<Word>> ord = new ArrayList<LinkedList<Word>>();
+		//
+		// for (Word w : words) {
+		// ord.add(map.get(w.getKey()));
+		// }
+		//
+		// System.out.println(ord.toString());
 		System.out.println(map.toString());
-		
+
 		Queue<Word> queue = new LinkedList();
-		queue.add(words.get(2));
-		System.out.println(words.get(2));
+		queue.add(words.get(8));
+		System.out.println(queue.peek());
 		while (!queue.isEmpty()) {
 			Word word = queue.remove();
 			Word child = null;
-			while ((child=map.get(word).pop()) != null) {
-				System.out.println(child);
-				queue.add(child);
+			try {
+				while (true) {
+					child = map.get(word).pop();
+					System.out.println(child);
+					queue.add(child);
+				}
+			} catch (NoSuchElementException e) {
+				
 			}
+
 		}
 	}
-	
+
 	public static class Node {
 		private LinkedList<Word> subNodes;
-		
+
 		public Node(LinkedList<Word> subNodes) {
 			this.subNodes = subNodes;
 		}
-		
+
 		public Word nextNode() {
-			if (!subNodes.isEmpty()){
+			if (!subNodes.isEmpty()) {
 				return subNodes.pop();
 			}
 			return null;
@@ -92,8 +98,6 @@ public class main {
 			sortedString = new String(unsortedChars);
 		}
 
-	
-
 		/**
 		 * check if otherString is contained inside this Word.
 		 * 
@@ -107,19 +111,22 @@ public class main {
 				if (indexOfChar == -1) {
 					return false;
 				}
-				tempString = tempString.substring(0, indexOfChar) + tempString.substring(1+indexOfChar);
+				tempString = tempString.substring(0, indexOfChar)
+						+ tempString.substring(1 + indexOfChar);
 			}
-			
+
 			return true;
 		}
-		public int hashCode(){
+
+		public int hashCode() {
 			return sortedString.hashCode();
 		}
+
 		public String getKey() {
 			return sortedString;
 		}
-		
-		public String toString(){
+
+		public String toString() {
 			return string;
 		}
 	}
