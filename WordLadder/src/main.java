2db2b8a8cccd12d6bc3db6a810965fa1ed488adb
Ludvik Brodl <ludvik.brodl@gmail.com>
2014-03-28 12:26:01 +0100

    bfs påbörjad

--- WordLadder/src/main.java
+++ WordLadder/src/main.java
@@ -22,11 +22,31 @@ public class main {
 		parse(arg);
 		System.out.println(words);
 		System.out.println(combinations);
+		
 		// build the graph. WORKS 10:42 28/3
 		buildGraph();
 		System.out.println(graph);
+		
+		//bfs on all combinations
+		for (int i = 0; i < combinations.size(); i = i + 2) {
+			int depth = bfs(graph, combinations.get(i), combinations.get(i + 1));
+		}
 	}
-	
+
+	private static int bfs(HashMap<String, LinkedList<String>> graph, String wordFrom,
+			String wordTo) {
+		HashMap<String, Boolean> visitedGraph = new HashMap<String, Boolean>();
+		for (String word : words) {
+			visitedGraph.put(word, false);
+		}
+		Queue queue = new LinkedList<String>();
+		queue.add(wordFrom);
+		while (!queue.isEmpty()) {
+			//GOGOGO
+		}
+		return 0;
+	}
+
 	/**
 	 * builds our graph from the words ArrayList(dictionary)
 	 */
@@ -38,17 +58,21 @@ public class main {
 			for (int j = 0; j < words.size(); j++) {
 				String nextWord = words.get(j);
 				if (i != j && checkMatch(word, nextWord)) {
-					linkedList.add(nextWord);					
+					linkedList.add(nextWord);
 				}
 			}
 		}
 	}
+
 	/**
-	 * check if word should have a link to nextWord. 
-	 * (4 last letters in word has to be contained in nextWord)
-	 * @param nextWord a link to this string?
-	 * @param word a link from this string?
-	 * @return 
+	 * check if word should have a link to nextWord. (4 last letters in word has
+	 * to be contained in nextWord)
+	 * 
+	 * @param nextWord
+	 *            a link to this string?
+	 * @param word
+	 *            a link from this string?
+	 * @return
 	 */
 	public static boolean checkMatch(String word, String nextWord) {
 		word = word.substring(1);
@@ -63,144 +87,148 @@ public class main {
 		return true;
 	}
 
-//		// lägg till alla 4:a bokstäver långa ord till bucketsen.
-//		Set<Node> keySet = map.keySet();
-//		for (Node key : keySet) {
-//			for (int i = 0; i < words.size(); i++) {
-//				Node word = words.get(i);
-//				if (word.checkMatch(key.getKey())
-//						&& !word.toString().equals(key.toString())) {
-//					map.get(key).add(word);
-//				}
-//			}
-//		}
-//		 System.out.println(map.toString());
-//		Iterator itr = combinations.iterator();
-//		
-//		while (itr.hasNext()) {
-//			distanceBetween(map, new Node(itr.next().toString()), new Node(itr.next().toString()));
-//			clear();
-//		}
-//
-//	}
-//
-//	private static void clear() {
-//		for (Node n : words) {
-//			n.setVisited(false);
-//		}
-//	}
-//
-//	public static void distanceBetween(HashMap<Node, LinkedList<Node>> graph,
-//			Node from, Node to) {
-//
-//		Queue<Node> queue = new LinkedList();
-//
-//		queue.add(from);
-//		from.setVisited(true);
-//		// System.out.println(queue.peek());
-//		int currentDepth = 0, elementsToDepthIncrease = 1, nextElementsToDepthIncrease = 0;
-//		while (!queue.isEmpty()) {
-//			Node word = queue.remove();
-//			Node child = null;
-//
-//			try {
-//				while (true) {
-//					LinkedList<Node> w = graph.get(word);
-//					child = w.pop();
-//					nextElementsToDepthIncrease += w.size();
-//					if (elementsToDepthIncrease == 0) {
-//
-//						++currentDepth;
-//						elementsToDepthIncrease = nextElementsToDepthIncrease;
-//						nextElementsToDepthIncrease = 0;
-//					}
-//					elementsToDepthIncrease--;
-//					if (!child.getVisited()) {
-//						if (child.toString().equals(to.toString())) {
-//							System.out.println(currentDepth);
-//							return;
-//						}
-//						queue.add(child);
-//						child.setVisited(true);
-//					}
-//				}
-//			} catch (NoSuchElementException e) {
-//
-//			}
-//
-//		}
-//		System.out.println("-1");
-//	}
-//
-//	public static class Node {
-//		private String string;
-//		private String[] sortedStrings;
-//		private boolean visited = false;
-//
-//		public Node(String string) {
-//			this.string = string;
-//
-//			sortedStrings = string.substring(1);
-//			char[] unsortedChars = sortedStrings.toCharArray();
-//
-//			Arrays.sort(unsortedChars);
-//			sortedStrings = new String(unsortedChars);
-//		}
-//
-//		public String[] getKeys() {
-//			Stri
-//			return null;
-//		}
-//
-//		public boolean getVisited() {
-//			return visited;
-//		}
-//
-//		public void setVisited(boolean b) {
-//			visited = b;
-//		}
-//
-//		/**
-//		 * check if otherString is contained inside this Word.
-//		 * 
-//		 * @param otherString
-//		 * @return
-//		 */
-//		public boolean checkMatch(String otherString) {
-//			String tempString = string;
-//			for (int i = 0; i < otherString.length(); i++) {
-//				int indexOfChar = tempString.indexOf(otherString.charAt(i));
-//				if (indexOfChar == -1) {
-//					return false;
-//				}
-//				tempString = tempString.substring(0, indexOfChar)
-//						+ tempString.substring(1 + indexOfChar);
-//			}
-//
-//			return true;
-//		}
-//
-//		public int hashCode() {
-//			return sortedStrings.hashCode();
-//		}
-//
-//		public String getKey() {
-//			return sortedStrings;
-//		}
-//
-//		public String toString() {
-//			return string;
-//		}
-//
-//		public boolean equals(Object o) {
-//			return sortedStrings.equals(((Node) o).sortedStrings);
-//		}
-//
-//	}
+	// // lägg till alla 4:a bokstäver långa ord till bucketsen.
+	// Set<Node> keySet = map.keySet();
+	// for (Node key : keySet) {
+	// for (int i = 0; i < words.size(); i++) {
+	// Node word = words.get(i);
+	// if (word.checkMatch(key.getKey())
+	// && !word.toString().equals(key.toString())) {
+	// map.get(key).add(word);
+	// }
+	// }
+	// }
+	// System.out.println(map.toString());
+	// Iterator itr = combinations.iterator();
+	//
+	// while (itr.hasNext()) {
+	// distanceBetween(map, new Node(itr.next().toString()), new
+	// Node(itr.next().toString()));
+	// clear();
+	// }
+	//
+	// }
+	//
+	// private static void clear() {
+	// for (Node n : words) {
+	// n.setVisited(false);
+	// }
+	// }
+	//
+	// public static void distanceBetween(HashMap<Node, LinkedList<Node>> graph,
+	// Node from, Node to) {
+	//
+	// Queue<Node> queue = new LinkedList();
+	//
+	// queue.add(from);
+	// from.setVisited(true);
+	// // System.out.println(queue.peek());
+	// int currentDepth = 0, elementsToDepthIncrease = 1,
+	// nextElementsToDepthIncrease = 0;
+	// while (!queue.isEmpty()) {
+	// Node word = queue.remove();
+	// Node child = null;
+	//
+	// try {
+	// while (true) {
+	// LinkedList<Node> w = graph.get(word);
+	// child = w.pop();
+	// nextElementsToDepthIncrease += w.size();
+	// if (elementsToDepthIncrease == 0) {
+	//
+	// ++currentDepth;
+	// elementsToDepthIncrease = nextElementsToDepthIncrease;
+	// nextElementsToDepthIncrease = 0;
+	// }
+	// elementsToDepthIncrease--;
+	// if (!child.getVisited()) {
+	// if (child.toString().equals(to.toString())) {
+	// System.out.println(currentDepth);
+	// return;
+	// }
+	// queue.add(child);
+	// child.setVisited(true);
+	// }
+	// }
+	// } catch (NoSuchElementException e) {
+	//
+	// }
+	//
+	// }
+	// System.out.println("-1");
+	// }
+	//
+	// public static class Node {
+	// private String string;
+	// private String[] sortedStrings;
+	// private boolean visited = false;
+	//
+	// public Node(String string) {
+	// this.string = string;
+	//
+	// sortedStrings = string.substring(1);
+	// char[] unsortedChars = sortedStrings.toCharArray();
+	//
+	// Arrays.sort(unsortedChars);
+	// sortedStrings = new String(unsortedChars);
+	// }
+	//
+	// public String[] getKeys() {
+	// Stri
+	// return null;
+	// }
+	//
+	// public boolean getVisited() {
+	// return visited;
+	// }
+	//
+	// public void setVisited(boolean b) {
+	// visited = b;
+	// }
+	//
+	// /**
+	// * check if otherString is contained inside this Word.
+	// *
+	// * @param otherString
+	// * @return
+	// */
+	// public boolean checkMatch(String otherString) {
+	// String tempString = string;
+	// for (int i = 0; i < otherString.length(); i++) {
+	// int indexOfChar = tempString.indexOf(otherString.charAt(i));
+	// if (indexOfChar == -1) {
+	// return false;
+	// }
+	// tempString = tempString.substring(0, indexOfChar)
+	// + tempString.substring(1 + indexOfChar);
+	// }
+	//
+	// return true;
+	// }
+	//
+	// public int hashCode() {
+	// return sortedStrings.hashCode();
+	// }
+	//
+	// public String getKey() {
+	// return sortedStrings;
+	// }
+	//
+	// public String toString() {
+	// return string;
+	// }
+	//
+	// public boolean equals(Object o) {
+	// return sortedStrings.equals(((Node) o).sortedStrings);
+	// }
+	//
+	// }
 
 	/**
 	 * parses 2 files to our words and combinations arrays
-	 * @param args file1 = dictionary, file2 = matches to check depth of.
+	 * 
+	 * @param args
+	 *            file1 = dictionary, file2 = matches to check depth of.
 	 * @throws IOException
 	 */
 	private static void parse(String[] args) throws IOException {
